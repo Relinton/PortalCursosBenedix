b7f22c551d5e2b938ec83063595824a0018ade3a
Relinton Pinheiro <relinton.pinheiro.infouniron@gmail.com>
2022-11-14 12:23:51 -0400

    Projeto DDD (Domain Drive Design) Arquitetura Orientada a Domínio em Asp Net Core Mvc

---
+++ PortalCursosBenedix.Application/Controllers/AdministrativoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortalCursosBenedix.Application.Controllers
+{
+    public class AdministrativoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
